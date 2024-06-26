3b069ab1a42c180feb9139f343874bc57d2b4556
bhavyaajith <bhavyaajith13@gmail.com>
2024-06-26 10:57:52 +0530

    Project created

---
+++ script.js
@@ -0,0 +1,106 @@
+console.log("Hello");
+console.warn("warn");
+console.error("error");
+
+
+// primitive data types
+
+var m = 10;        //length
+console.log(m);
+
+
+
+let namee = "Yellow";   // Strings
+let lastName = "Red";
+console.log(namee);
+
+
+
+
+const name ="bhv";       //object
+const age = 12;
+
+
+console.log('my name is' + name + 'and age is' +age);
+
+console.log('my name is ${name} and age is ${age} ');
+
+
+// Array object:
+arrayOne = ["john" ,30, true, null, undefined]
+
+console.log(arrayOne);
+
+arrayOne.push("Apple");
+console.log(arrayOne);
+
+console.log(arrayOne[0]);
+
+arrayOne[3]= "banana"
+console.log(arrayOne);
+
+console.log(arrayOne.length);
+arrayOne.splice(3,2);
+console.log(arrayOne);
+
+//arithmetic operators
+var c = 3;
+var d= c + 2;
+console.log(d);
+
+//assignment operator
+let  s = 3
+let  w = 5
+s = w;
+console.log(s);
+
+
+ document.getElementById('Box').addEventListener('click',function(){
+    alert("I got clicked");
+ })
+
+ document.getElementById('Css').addEventListener('click',function(){
+    alert("dbclicked");
+})
+
+
+console.log("Hello World");
+console.log("HAiii");
+
+var a =10;
+console.log(a);
+
+const pi=3.14;
+console.log(pi);
+
+let valueOne = 2;
+let valueTwo = "2";
+
+console.log(valueOne+valueTwo);
+console.log(valueOne + parseInt(valueTwo));
+
+
+
+
+
+
+let valueOnee ="2"
+let valuetwo = 2;
+
+console.log(valueOnee== valueTwo)
+
+let value = 18;
+
+if(value >= 18)
+   {
+      console.log("You are adult");
+   }
+   else{
+      console.log("minor");
+
+   }
+
+   let agee = 20;
+   let type = (age >= 18) ? "Adult": "Minor"
+   console.log(type);
+
